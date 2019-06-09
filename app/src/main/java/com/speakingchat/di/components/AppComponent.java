b071f095df3dd7305a6027fda3b7df00b144e13d
Marya Podolyak <ma-p9@mail.ru>
2019-06-10 00:58:33 +0300

    Create Api, network scheme

--- app/src/main/java/com/speakingchat/di/components/AppComponent.java
+++ app/src/main/java/com/speakingchat/di/components/AppComponent.java
@@ -5,15 +5,19 @@ import com.speakingchat.di.annotations.ApplicationScope;
 import com.speakingchat.di.module.AppContextModule;
 import com.speakingchat.di.module.GoogleApiClientModule;
 import com.speakingchat.di.module.PreferencesModule;
+import com.speakingchat.di.module.RetrofitModule;
 import com.speakingchat.views.activities.MainActivity;
 
 import dagger.Component;
+import retrofit2.Retrofit;
 
 @ApplicationScope
-@Component(modules = {AppContextModule.class, PreferencesModule.class})
+@Component(modules = {AppContextModule.class, PreferencesModule.class, RetrofitModule.class})
 public interface AppComponent {
 
 	void inject(MainActivity activity);
 
 	SignInComponent createSignInComponent(GoogleApiClientModule module);
+
+	Retrofit getRetrofit();
 }
